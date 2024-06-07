7cf3aca888331ceb8eaa6c45ad4369da6f505a9f
rahimbek027 <xabibullayevrahimbek5@gmail.com>
2024-06-07 06:36:07 +0500

    message js failes

---
+++ js/app.js
@@ -0,0 +1,183 @@
+
+//masala 1
+  {
+    let son = +prompt("son kiriting")
+
+    if (son>0)
+    {  console.log(son = son + 1)}
+    else{
+        console.log(son)
+    }
+} 
+
+
+// masala 2
+
+ /*{
+    let son = +prompt("son kiriting")
+
+    if (son>0)
+    {  console.log(son = son + 1)}
+    else{
+        console.log(son = son - 2)
+    } 
+}*/
+
+
+// masala 3
+
+ /*{
+    let son = +prompt("son kiriting");
+
+if (son > 0) {
+    console.log(son + 1);
+} else if (son < 0) { 
+    console.log(son - 2);
+} else {
+    console.log(10);
+}
+
+
+}*/
+
+
+
+// masala 4
+
+/*{
+ let a = +prompt("a ni kiriting");
+ let b = +prompt("b ni kiriting");
+ let c = +prompt("c ni kiriting");
+
+ if (a>0 && b>0 && c>0){
+    console.log("3 ta musbat son bor");
+ } else if (a>0 && b>0 && c<0 || a>0 && b<0 && c>0 || a<0 && b>0 && c>0){
+    console.log("2 ta musbat son bor");
+ }else if (a>0 && b<0 && c<0 || a<0 && b>0 && c<0 || a<0 && b<0 && c>0){
+    console.log("1 ta musbat son bor")
+ }else{
+    console.log("siz kiritgan sonlar ichida 1 ta ham musbat son yo'q")
+    
+ }
+
+}*/
+
+
+// masala 5 
+
+/*{
+ let a = +prompt("a ni kiriting");
+ let b = +prompt("b ni kiriting");
+ let c = +prompt("c ni kiriting");
+
+
+ if (a>0 && b>0 && c>0){
+    console.log("3 ta musbat son bor");
+ } else if (a>0 && b>0 && c<0 || a>0 && b<0 && c>0 || a<0 && b>0 && c>0){
+    console.log("2 ta musbat va 1 ta manfiy son bor");
+ }else if (a>0 && b<0 && c<0 || a<0 && b>0 && c<0 || a<0 && b<0 && c>0){
+    console.log("1 ta musbat va 2 ta manfiy  son bor")
+ }else{
+    console.log("3 ta manfiy son bor")
+ }
+   
+}*/
+
+
+
+//masala 6
+
+
+/*{
+    let a = +prompt("a ni kiriting");
+    let b = +prompt("b ni kiriting");
+
+    if (a>b){
+        console.log(a);
+    }else{
+        console.log(b)
+    }
+}*/
+
+
+// masala 7
+
+/*{
+    let a = +prompt("a ni kiriting");
+    let b = +prompt("b ni kiriting");
+
+    if (a>b){
+        console.log("kichik sonning tartib raqami :", b ,"ga teng");
+    }else{
+        console.log("kichik sonning tartib raqami :", a ,"ga teng")
+    }
+}*/
+
+
+//masala8
+/*{
+    let a = +prompt("a ni kiriting");
+    let b = +prompt("b ni kiriting");
+     
+    if (a>b){
+        console.log("katta son", a,";kichik son",b);
+    }else{
+        console.log("katta son", b,"kichik son",a)
+    }
+
+}*/
+
+
+//masala 9
+/* masala shartiga tushunmadm */
+
+//masala 10
+
+/*{
+    let a = +prompt("a ni kiriting");
+    let b = +prompt("b ni kiriting");
+
+    if (a!=b){
+        console.log("A =",a*2, "B =",b*2)
+    }else{
+        console.log("A =",0,  "B =",0)
+    }
+}*/
+
+
+
+//masala 11
+
+ /*{
+    let a = +prompt("a ni kiriting");
+    let b = +prompt("b ni kiriting");
+
+    if (a>b){
+        console.log("A =",a, "B =",a)
+    }else if (a<b){
+        console.log("A =",b, "B =",b)
+    }else{
+        console.log("A =",0, "B =",0)
+    }
+} */
+
+    //masala 12 
+
+ /*   {
+        let a = +prompt("a ni kiriting");
+        let b = +prompt("b ni kiriting");
+        let c = +prompt("c ni kiriting");
+        
+        if (a>c && b>c){
+            console.log("kichik son =",c)
+        } else if (a>b && c>b){
+            console.log("kichik son = ",b)
+        }else{
+            console.log("kichik son = ",a)
+        }
+       
+   }*/
+
+   
+
+
